75833990ba1bc19b99e01d51b18427fd25e9bcfb
Zyrkeroleg <zyrker@mail.ru>
2022-07-24 13:42:37 +0300

    upload pagination

--- my_site/news/forms.py
+++ my_site/news/forms.py
@@ -7,14 +7,15 @@ from django.core.exceptions import ValidationError
 class NewsForm(forms.ModelForm):
     class Meta:
         model = News
-        fields = ('title', 'content', 'is_published', 'category')
+        fields = ("title", "content", "is_published", "category")
         widgets = {
-            'title': forms.TextInput(attrs={'class': 'form-control'}),
-            'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 5}),
-            'category': forms.Select(attrs={'class': 'form-control'}),
+            "title": forms.TextInput(attrs={"class": "form-control"}),
+            "content": forms.Textarea(attrs={"class": "form-control", "rows": 5}),
+            "category": forms.Select(attrs={"class": "form-control"}),
         }
+
     def clean_title(self):
-        title = self.cleaned_data['title']
-        if re.match(r'\d', title):
-            raise ValidationError('Название не должно начинаться с цифры')
+        title = self.cleaned_data["title"]
+        if re.match(r"\d", title):
+            raise ValidationError("Название не должно начинаться с цифры")
         return title
--- my_site/news/views.py
+++ my_site/news/views.py
@@ -1,9 +1,7 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
 from .models import News, Category
 from .forms import NewsForm
 from django.views.generic import ListView, DetailView, CreateView
-from django.urls import reverse_lazy
 from .utils import MyMixin
 from django.core.paginator import Paginator
 from django.contrib.auth.forms import UserCreationForm
@@ -11,67 +9,81 @@ from django.contrib import messages
 
 
 def register(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            messages.success(request, 'Вы успешно зарегистрировались')
-            return redirect('login')
+            messages.success(request, "Вы успешно зарегистрировались")
+            return redirect("login")
         else:
-            messages.error(request, 'Ошибка регистрации')
+            messages.error(request, "Ошибка регистрации")
     else:
         form = UserCreationForm()
-    return render(request, 'news/register.html', {'form': form})
+    return render(request, "news/register.html", {"form": form})
+
 
 def login(request):
-    return render(request, 'news/login.html')
+    return render(request, "news/login.html")
+
 
 class HomeNews(MyMixin, ListView):
     model = News
-    template_name = 'news/index.html'
-    context_object_name = 'news'
-    mixin_prop = 'Hello world'
+    template_name = "news/index.html"
+    context_object_name = "news"
     paginate_by = 2
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = 'Главная'
-        context['mixin_prop'] = self.get_prop()
+        context["title"] = "Главная"
+        context["mixin_prop"] = self.get_prop()
         return context
 
     def get_queryset(self):
-        return News.objects.filter(is_published=True).select_related('category')
+        return News.objects.filter(is_published=True).select_related("category")
 
 
 class News_by_category(MyMixin, ListView):
     model = News
-    template_name = 'news/index.html'
-    context_object_name = 'news'
+    template_name = "news/index.html"
+    context_object_name = "news"
     allow_empty = False
+    paginate_by = 2
 
     def get_queryset(self):
-        return News.objects.filter(category_id=self.kwargs['category_id'], is_published=True).select_related('category')
+        return News.objects.filter(
+            category_id=self.kwargs["category_id"], is_published=True
+        ).select_related("category")
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = Category.objects.get(pk=self.kwargs['category_id'])
+        context["title"] = Category.objects.get(pk=self.kwargs["category_id"])
         return context
+
 
 def get_category(request, category_id):
     news = News.objects.filter(category_id=category_id)
     category = Category.objects.get(pk=category_id)
-    return render(request, 'news/category.html', {'news': news,'category': category,})
+    return render(
+        request,
+        "news/category.html",
+        {
+            "news": news,
+            "category": category,
+        },
+    )
+
 
 class ViewNews(DetailView):
     model = News
-    template_name = 'news/view_news.html'
-    context_object_name = 'news'
+    template_name = "news/view_news.html"
+    context_object_name = "news"
 
     def get_queryset(self):
         return News.objects.filter()
-    context_object_name = 'news_item'
+
+    context_object_name = "news_item"
 
 
 class CreateNews(CreateView):
     form_class = NewsForm
-    template_name = 'news/add_news.html'
+    template_name = "news/add_news.html"
